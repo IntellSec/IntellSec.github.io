4c7f4af123c775b0282c62ea5b6bb262daa52c48
M <faymatt96@gmail.com>
2018-11-19 03:02:18 -0500

    comment bad code

--- js/script.js
+++ js/script.js
@@ -30,22 +30,22 @@ $(document).ready(function() {
     //   })
 
    // displays menu items for mobile view
-   var navScrollTop = $("nav").offset().top;
+  //  var navScrollTop = $("nav").offset().top;
   
-   function navScroll() {
-     var scrollTop = $(window).scrollTop();
+  //  function navScroll() {
+  //    var scrollTop = $(window).scrollTop();
  
-     if (scrollTop > navScrollTop) {
-       $("nav").addClass("sticky");
-     } else {
-       $("nav").removeClass("sticky");
-     }
-   }
-   navScroll();
+  //    if (scrollTop > navScrollTop) {
+  //      $("nav").addClass("sticky");
+  //    } else {
+  //      $("nav").removeClass("sticky");
+  //    }
+  //  }
+  //  navScroll();
  
-   $(window).scroll(function() {
-     navScroll();
-   });
+  //  $(window).scroll(function() {
+  //    navScroll();
+  //  });
  
    // Media query displays menu items for mobile view
    $(".handle").on("click", function() {
